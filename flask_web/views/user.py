7b39c047bb65c9b390599bc91c828d47b2e001c6
xue947441187 <34952466+xue947441187@users.noreply.github.com>
2020-02-13 18:37:18 +0800

    flask中增加注册类视图 vue中调整登录界面

--- flask_web/views/user.py
+++ flask_web/views/user.py
@@ -1,12 +1,13 @@
+import re
 import time
 
-from flask import request, jsonify, make_response
+from flask import request, jsonify, make_response, Response
 from flask.views import MethodView
 from flask_web.models.user_model import User
 from flask_web.config.config import parameter
 from flask_web.views.tools import encryption, login_auth, get_jwt
 
-from flask_web import app, Blueprint
+from flask_web import app, Blueprint, db
 
 user_views = Blueprint("user", __name__)
 
@@ -40,9 +41,9 @@ class LoginView(MethodView):
             current_user = User.query.filter_by(user_id=user_id).first()
         except Exception as e:
             app.logger.error(e)
-            return response({"error": parameter["not_user"]})
+            return jsonify({"error": parameter["not_user"]})
         if encryption(user_pwd) != current_user.password:
-            return response({"error": parameter['not_pwd']})
+            return jsonify({"error": parameter['not_pwd']})
         jwt_dict = {
             "iat": time.time(),
             'id': current_user.user_id
@@ -56,7 +57,46 @@ class LoginView(MethodView):
 
 class RegisteView(MethodView):
     def post(self):
-        ref = {
-            "success": parameter["login_success"]
-        }
-        return jsonify(ref)
+        # response = Response()
+        # response.headers["Access-Control-Allow-Origin"] = "*"
+        # response.headers["Access-Control-Allow-Methods"] = "POST"
+        # response.headers["Access-Control-Allow-Headers"] = "x-requested-with"
+
+        # request.headers.get("Access-Control-Allow-Origin","*")
+        if request.method == 'POST':
+            try:
+                # response.headers('Access-Control-Allow-Origin','*')
+                post = request.form
+                username = post['username']
+                userpassword = post['userpwd']
+                email = post['eamil']
+                phone = post['phone']
+                name = post['name']
+
+                re_eamil = re.match(r".*\@(163|qq)\.com", email).string
+                re_username = re.match(r'(\s|\d|_){6,}', username).string
+                re_userpassword = re.match(r'^\w+$', userpassword).string
+
+                re_phone = re.match(r'^1[3|4|5|8][0-9]\d{4,8}$', phone).string
+            except Exception as e:
+                return jsonify({"error": e})
+            try:
+                user = User.query.filter_by(username=username).first()
+            except Exception as e:
+                user = False
+            if user:
+                return jsonify({"success": "当前用户已被占用"}, 404)
+            re_name = re.match(r'(\s|\d|_)*', name).string
+            if not re_name:
+                re_name = re_username
+            if re_eamil == email and \
+                    re_username == username and \
+                    re_name == name and \
+                    re_userpassword == userpassword \
+                    and re_phone == phone:
+                if not User.query.filter_by(username=username).first():
+                    user = User(username=username, passwrod=userpassword, email=email, phone=phone, name=name)
+                    db.session.add(user)
+                    db.session.commit()
+                    return jsonify({"success": parameter['create_user_success']},200)
+        return jsonify({"error": parameter['create_user_error']}, 404)
--- vue-web/src/login/login.vue
+++ vue-web/src/login/login.vue
@@ -1,48 +1,73 @@
 <template>
-  <div class="login">
-   <div class="container">
+    <div class="login">
+        <div class="container">
+            <div class="modal-dialog" style="margin-top: 10%;">
+                <div class="modal-content">
+                    <div class="modal-header">
 
+                        <h4 class="modal-title text-center" id="myModalLabel">登录</h4>
+                    </div>
+                    <div class="modal-body" id="model-body">
+                        <div class="form-group">
 
+                            <input v-model="Upfrom.name" type="text" class="form-control" placeholder="用户名" autocomplete="off">
+                        </div>
+                        <div class="form-group">
 
-    <div class="modal-dialog" style="margin-top: 10%;">
-        <div class="modal-content">
-            <div class="modal-header">
+                            <input v-model="Upfrom.password" type="password" class="form-control" placeholder="密码" autocomplete="off">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="form-group">
+                            <button @click="this.loginUp" type="button" class="btn btn-primary form-control">登录</button>
+                        </div>
+                        <div class="form-group">
+                            <button type="button" class="btn btn-default form-control" @click="this.Upregiste">注册</button>
+                        </div>
 
-                <h4 class="modal-title text-center" id="myModalLabel">登录</h4>
-            </div>
-            <div class="modal-body" id = "model-body">
-                <div class="form-group">
-
-                    <input type="text" class="form-control" placeholder="用户名" autocomplete="off">
-                </div>
-                <div class="form-group">
-
-                    <input type="password" class="form-control" placeholder="密码" autocomplete="off">
-                </div>
-            </div>
-            <div class="modal-footer">
-                <div class="form-group">
-                    <button type="button" class="btn btn-primary form-control">登录</button>
-                </div>
-                <div class="form-group">
-                    <button type="button" class="btn btn-default form-control" @click="this.loadFro" >注册</button>
-                </div>
-
-            </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal -->
-
-
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal -->
+        </div>
     </div>
-
-  </div>
 </template>
 
 <script>
-  export default {
-    name: "login",
-
-  }
+    export default {
+        name: "login",
+        data() {
+            return {
+                  Upfrom:{
+                      name: "",
+                      password:""
+                  }
+            }
+        },
+        methods: {
+            Upregiste: function(){
+                this.location.href='/registe'
+            },
+            loginUp: function () {
+                var date = {
+                    "userid": this.Upfrom.name,
+                    "password": this.Upfrom.password
+                };
+                this.$axios({
+                    method: "POST",
+                    url: "/api" + "/login",
+                    dataType: "jsonp",
+                    data: this.qs.stringify(date)
+                }).then((data) => {
+                    if (data.data.code == 200 ){
+                        alert("成功");
+                        window.console.log(data.data)
+                    } else {
+                        alert("账号或者密码输入错误")
+                    }
+                }).catch()
+            }
+        }
+    }
 </script>
 
 <style scoped>
